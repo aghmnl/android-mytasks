d65479b77b3ec84b23cfbd1cdf677cb947ceebec
aghmnl <aghmnl@gmail.com>
2024-12-26 09:00:54 -0300

    New compose and kps libraries versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,9 @@ android {
     buildFeatures {
         compose = true
     }
-//    composeOptions {
-//        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-//    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
