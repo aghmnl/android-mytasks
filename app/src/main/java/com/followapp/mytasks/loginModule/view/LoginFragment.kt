ac43c9a172d953e891700399d48b6947623c6203
aghmnl <aghmnl@gmail.com>
2025-03-20 17:17:03 -0300

    View binding

--- app/src/main/java/com/followapp/mytasks/loginModule/view/LoginFragment.kt
+++ app/src/main/java/com/followapp/mytasks/loginModule/view/LoginFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.followapp.mytasks.R
+import com.followapp.mytasks.databinding.FragmentLoginBinding
 import com.followapp.mytasks.homeModule.view.HomeFragment
 import com.followapp.mytasks.loginModule.model.LoginRepository
 import com.followapp.mytasks.loginModule.viewModel.LoginViewModel
@@ -17,10 +17,14 @@ import com.followapp.mytasks.loginModule.viewModel.LoginViewModelFactory
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,7 +32,7 @@ class LoginFragment : Fragment() {
 
         setupViewModel()
         setupObservers()
-        setupListeners(view)
+        setupListeners()
     }
 
     private fun setupViewModel() {
@@ -58,9 +62,14 @@ class LoginFragment : Fragment() {
         })
     }
 
-    private fun setupListeners(view: View) {
-        view.findViewById<Button>(R.id.signInGoogle).setOnClickListener {
+    private fun setupListeners() {
+        binding.signInGoogle.setOnClickListener {
             loginViewModel.checkPlayServices(requireContext())
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
