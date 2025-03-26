6db35960a2b4362714ddc4147f71c12e62242e26
aghmnl <aghmnl@gmail.com>
2025-03-26 18:00:40 -0300

    LoginScreen created with Preview

--- app/src/main/java/com/followapp/mytasks/loginModule/view/LoginFragment.kt
+++ app/src/main/java/com/followapp/mytasks/loginModule/view/LoginFragment.kt
@@ -5,35 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.followapp.mytasks.R
-import com.followapp.mytasks.databinding.FragmentLoginBinding
 import com.followapp.mytasks.homeModule.view.HomeFragment
 import com.followapp.mytasks.loginModule.viewModel.LoginViewModel
 import org.koin.android.ext.android.inject
 
 class LoginFragment : Fragment() {
 
-    private var _binding: FragmentLoginBinding? = null
-    private val binding get() = _binding!!
-
     private val loginViewModel: LoginViewModel by inject()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupObservers()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setupObservers()
-        setupListeners()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                LoginScreen(
+                    onSignInClick = { loginViewModel.signInWithGoogle(requireContext()) }
+                )
+            }
+        }
     }
 
     private fun setupObservers() {
-        loginViewModel.user.observe(viewLifecycleOwner, Observer { user ->
+        loginViewModel.user.observe(this, Observer { user ->
             if (user != null) {
                 parentFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container_view, HomeFragment())
@@ -42,19 +42,8 @@ class LoginFragment : Fragment() {
             }
         })
 
-        loginViewModel.errorMessage.observe(viewLifecycleOwner, Observer { message ->
+        loginViewModel.errorMessage.observe(this, Observer { message ->
             Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
         })
-    }
-
-    private fun setupListeners() {
-        binding.signInGoogle.setOnClickListener {
-            loginViewModel.signInWithGoogle(requireContext())
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
