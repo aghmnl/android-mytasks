da4ab3fd40d5df20b5145017f773ca8cefca1cf6
aghmnl <aghmnl@gmail.com>
2025-03-26 15:37:29 -0300

    initializeAdView moved to the repository

--- app/src/main/java/com/followapp/mytasks/adModule/model/AdRepository.kt
+++ app/src/main/java/com/followapp/mytasks/adModule/model/AdRepository.kt
@@ -1,11 +1,20 @@
 package com.followapp.mytasks.adModule.model
 
+import android.content.Context
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.LoadAdError
 
 class AdRepository {
+
+    fun initializeAdView(context: Context, adUnitId: String): AdView {
+        val adView = AdView(context)
+        adView.adUnitId = adUnitId
+        adView.setAdSize(AdSize.BANNER)
+        return adView
+    }
 
     fun loadAd(adView: AdView, onAdLoaded: () -> Unit, onAdFailed: () -> Unit) {
         val adRequest = AdRequest.Builder().build()
--- app/src/main/java/com/followapp/mytasks/adModule/viewModel/AdViewModel.kt
+++ app/src/main/java/com/followapp/mytasks/adModule/viewModel/AdViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.followapp.mytasks.adModule.model.AdRepository
-import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 
 class AdViewModel(private val repository: AdRepository) : ViewModel() {
@@ -19,10 +18,7 @@ class AdViewModel(private val repository: AdRepository) : ViewModel() {
     fun setAdLoaded(value: Boolean) = _adLoaded.postValue(value)
 
     fun initializeAdView(context: Context, adUnitId: String) {
-        val adView = AdView(context)
-        adView.adUnitId = adUnitId
-        adView.setAdSize(AdSize.BANNER)
-        _adView.value = adView
+        _adView.value = repository.initializeAdView(context, adUnitId)
     }
 
     fun loadAd() {
